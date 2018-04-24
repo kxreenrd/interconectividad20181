2b91dd8ae5c809b1d93c74aead42fc73f1657115
ISwMF <31453580+ISwMF@users.noreply.github.com>
2018-04-24 18:10:31 -0500

    Cliente

--- cliente.py
+++ cliente.py
@@ -1,24 +1,24 @@
 
+
 import socket, pickle
 import json
 
 HOST = 'localhost'
 PORT = 50007
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((HOST, PORT))
+s.connect(('10.8.8.59', PORT))
 s.settimeout(10.0)
 i=0
 while True:
-    cadena = raw_input("Introduce el mensaje: ")
+    cadena = input("Introduce el mensaje: ")
     cadena = cadena.encode('utf-8')
     s.send(cadena)
     i+=1
     if (cadena=="get"):
-        #data = "ome gonorrea ome, pablito escobar ome, aguante Nacional ome, Uribe el mejor presidente duelale a quien le duela ome"
-        #data = json.dumps({"0":"dato1", "1": "dato2", "2":"dato3"})
-        #s.send(data.encode())
+        print("hola")
     if (cadena=="salir"):
         data = s.recv(4096)
         print(data)
         break
         s.close()
+
