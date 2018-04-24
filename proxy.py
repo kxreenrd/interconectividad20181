54ad6920905ab2ed77f2e087568c0db678324b22
ISwMF <31453580+ISwMF@users.noreply.github.com>
2018-04-24 18:04:54 -0500

    Subido con IP de la red UNIMINUTO

--- proxy.py
+++ proxy.py
@@ -4,26 +4,29 @@ PORT = 50007
 
 #Socket de cliente
 sC = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-sC.connect((HOST, PORT))
+sC.connect(('10.8.8.58', PORT))
 #Supongo que aquí se coloca la dirección del servidor
 #El proxy actua como cliente ante el servidor
 
 #Socket de servidor
 sS = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-sS.bind((HOST, PORT))
+sS.bind(('10.8.8.59', PORT))
 #Y aquí el del cliente
 #El proxy actua como servidor ante el cliente
 sS.listen(3)
+conn, addr = sS.accept()
 
 while True:
     #Se supone que se tiene que escuchar al cliente
     data = conn.recv(4096)
     data = data.decode("ascii")
+    print(data)
     #Si el cliente envia la palabra get
     if (data == "get"):
         obtener = "get"
         obtener = obtener.encode('utf-8')
         #Se envia la palabra get al servidor
         sC.send(obtener)
-        #Se recibe y se decodifica el paquete que el servido envia 
-        paquete = sS.recv(4096)
+        #Se recibe y se decodifica el paquete que el servido envia
+        paquete = sC.recv(4096)
+        print(paquete)
