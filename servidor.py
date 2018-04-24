31fd42863b6874a57dd8e98fab5873443b6538a7
ISwMF <31453580+ISwMF@users.noreply.github.com>
2018-04-24 18:06:40 -0500

    Da una respuesta "prueba"

--- servidor.py
+++ servidor.py
@@ -5,7 +5,7 @@ import json
 HOST = 'localhost'
 PORT = 50007
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind((HOST, PORT))
+s.bind(('10.8.8.58', PORT))
 s.listen(3)
 conn, addr = s.accept()
 print ('Connected by', addr)
@@ -15,23 +15,14 @@ while True:
     print(data)
     if (data == "salir"):
         conn.send(data)
-
         print ("Adios")
         conn.close()
         break
+
     if (data == "get"):
-        #enviar = "prueba"
-        #conn.send(enviar)
-        i=0
-        while i != 10:
-            data = json.dumps({i: "palabra"})
-            conn.send(data)
-            i=+1
-        #data = json.dumps({"0":"dato1", "1": "dato2", "2":"dato3"})
-        #data2 = conn.recv(4096)
-        #data2 = json.loads(data2.decode())
-        #conn.send("Usted ha enviado: ")
-        #print(data2.get("0"))
-        #print("hola")
+        enviar = "prueba"
+        enviar = enviar.encode('utf-8')
+        conn.send(enviar)
+
 conn.close()
 s.close()
