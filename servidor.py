3c54a72e5a81eb702de21c6ecacbe4af9f7950a9
ISwMF <31453580+ISwMF@users.noreply.github.com>
2018-04-24 21:39:30 -0500

    Falta hacer el checksum y mandar una a una

--- servidor.py
+++ servidor.py
@@ -2,12 +2,16 @@ import socket
 import time
 import json
 
+frase = "este proxy no es malicioso y pasa la prueba bien"
 HOST = 'localhost'
 PORT = 50007
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind(('10.8.8.58', PORT))
+s.bind(('10.8.8.57', PORT))
 s.listen(3)
 conn, addr = s.accept()
+#Lista para las palabras con checksum
+listaPalabras = []
+
 print ('Connected by', addr)
 while True:
     data = conn.recv(4096)
@@ -26,3 +30,4 @@ while True:
 
 conn.close()
 s.close()
+
