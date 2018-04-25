f9d447e98205b1184f5bc8a766efd991ab62bf78
ISwMF <31453580+ISwMF@users.noreply.github.com>
2018-04-24 21:44:30 -0500

    Falta corromper un paquete y eliminar alguno

--- proxy.py
+++ proxy.py
@@ -4,13 +4,13 @@ PORT = 50007
 
 #Socket de cliente
 sC = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-sC.connect(('10.8.8.58', PORT))
+sC.connect(('10.8.8.57', PORT))
 #Supongo que aquí se coloca la dirección del servidor
 #El proxy actua como cliente ante el servidor
 
 #Socket de servidor
 sS = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-sS.bind(('10.8.8.59', PORT))
+sS.bind(('10.8.8.58', PORT))
 #Y aquí el del cliente
 #El proxy actua como servidor ante el cliente
 sS.listen(3)
@@ -29,4 +29,6 @@ while True:
         sC.send(obtener)
         #Se recibe y se decodifica el paquete que el servido envia
         paquete = sC.recv(4096)
+
+        conn.send(paquete)
         print(paquete)
