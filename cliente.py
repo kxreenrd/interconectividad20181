2ecc980928fd3e3f26417ac27e9cc6d9366321a1
ISwMF <31453580+ISwMF@users.noreply.github.com>
2018-04-24 21:45:56 -0500

    Falta verificar las palabras que llegan

--- cliente.py
+++ cliente.py
@@ -6,19 +6,28 @@ import json
 HOST = 'localhost'
 PORT = 50007
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(('10.8.8.59', PORT))
+s.connect(('10.8.8.58', PORT))
 s.settimeout(10.0)
 i=0
 while True:
     cadena = input("Introduce el mensaje: ")
     cadena = cadena.encode('utf-8')
     s.send(cadena)
-    i+=1
+    palabras = []
+    i=0
+    #while para recivir las palabras
+    while i!=2:
+        palabras.append(s.recv(4096))
+        i=+1
+
+    #data = s.recv(4096)
+    #data = data.decode('ascii')
+    #print(data)
     if (cadena=="get"):
-        print("hola")
+        data = s.recv(4096)
+        print(data)
     if (cadena=="salir"):
         data = s.recv(4096)
         print(data)
         break
         s.close()
-
