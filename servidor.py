81615dbace9b62e8919584a96ea2ee140feab150
ISwMF <31453580+ISwMF@users.noreply.github.com>
2018-04-25 13:11:12 -0500

    El servidor está completo

--- servidor.py
+++ servidor.py
@@ -2,15 +2,37 @@ import socket
 import time
 import json
 
+###Las palabras en este proyecto no tienen tildes ni caracteres especiales por la codificacion ASCII
+
+#Frase de 10 palabras que se va a enviar
 frase = "este proxy no es malicioso y pasa la prueba bien"
+
+#Lista para las palabras con numeracion y checksum
+listaPalabras = []
+
+#<---Inicio del checksum--->#
+#Obtener el checksum de las palabras puestas en 'frase'
+separacion= frase.split(" ")
+for palabra in separacion:
+    acumulador=0
+    for caracter in palabra:
+        caracterp = int(format(ord(caracter), 'b'),2)
+        acumulador = acumulador^caracterp
+    inverso = ~acumulador & 0xFF
+
+    #se anade a la lista tres datos en notacion JSON, estos datos son: numero, palabra y checksum
+    #Ejemplo: {"checksum": "11111000", "palabra": "este", "numero": 0}
+    listaPalabras.append(json.dumps({"numero":len(listaPalabras),"palabra":palabra, "checksum": format(inverso,'b')}))
+#<---Fin del checksum--->#
+
 HOST = 'localhost'
 PORT = 50007
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind(('10.8.8.57', PORT))
+
+#En HOST se coloca la IP de este computador
+s.bind((HOST, PORT))
 s.listen(3)
 conn, addr = s.accept()
-#Lista para las palabras con checksum
-listaPalabras = []
 
 print ('Connected by', addr)
 while True:
@@ -22,12 +44,14 @@ while True:
         print ("Adios")
         conn.close()
         break
-
     if (data == "get"):
-        enviar = "prueba"
-        enviar = enviar.encode('utf-8')
-        conn.send(enviar)
+        print ("Se enviaran las palabras")
+        #Se envia las palabras colocaldas en 'listaPalabras'
+        for LP in listaPalabras:
+            conn.send(LP.encode())
+            #Espera 0.2 segundos para que el cliente pueda hacer su proceso
+            #Si no se hace, el cliente se sale por TimeOut
+            time.sleep(0.2)
 
 conn.close()
 s.close()
-
